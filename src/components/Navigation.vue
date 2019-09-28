8474233805eee00499a8961a22f550213f81620b
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-09-28 16:07:38 -0400

    Remove duplicate link

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,7 +4,6 @@
         <div class="container-fluid">
             <router-link class="navbar-brand" to="/">Conference Log</router-link>
             <div class="navbar-nav ml-auto">
-                <router-link class="nav-item nav-link" to="/login">Login</router-link>
                 <router-link class="nav-item nav-link" to="/conferences">Conferences</router-link>
                 <router-link class="nav-item nav-link" to="/login">Login</router-link>
                 <router-link class="nav-item nav-link" to="/register">Register</router-link>
