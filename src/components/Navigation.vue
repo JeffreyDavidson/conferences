4e281f892be32b3e28ce72068574123c8961b621
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-09-28 16:06:38 -0400

    Add navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,15 @@
 <template>
-    <div>Navigation</div>
+  <div>
+    <nav class="site-nav navbar navbar-expand bg-primary navbar-dark">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" to="/">Conference Log</router-link>
+            <div class="navbar-nav ml-auto">
+                <router-link class="nav-item nav-link" to="/login">Login</router-link>
+                <router-link class="nav-item nav-link" to="/conferences">Conferences</router-link>
+                <router-link class="nav-item nav-link" to="/login">Login</router-link>
+                <router-link class="nav-item nav-link" to="/register">Register</router-link>
+            </div>
+        </div>
+    </nav>
+  </div>
 </template>
