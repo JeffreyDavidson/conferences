d96996ac4e916c4c95a79fd8df8e6903627f3ca6
Jefffrey Davidson <jeffrey.davidson@outlook.com>
2020-01-30 12:41:48 -0500

    Adding in data calls from firebase database

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
     <Navigation />
-    <router-view class="container"/>
+    <router-view class="container" :user="user" />
   </div>
 </template>
 
 <script>
 import Navigation from "@/components/Navigation.vue";
+import Firebase from "firebase";
+import db from "./db.js";
+
 export default {
   name: "app",
+  data: function() {
+    return {
+      user: null
+    }
+  },
   components: {
     Navigation
-  }
-}
+  },
+  mounted() {
+    db.collection("users")
+      .doc("4NzY9NE4j3TOtt2z8ar6")
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name;
+      });
+  },
+};
 </script>
 
 <style lang="scss">
